9fd16362d9e62be438aefb30c4e168daf0a6e8e5
Rafael Ulisses <ruds.devel@gmail.com>
2023-04-29 19:02:02 -0300

    [FIX] - Company.save implemented on Repo instead Service

--- src/main/java/br/org/ruds/repositorys/CompanyRepository.java
+++ src/main/java/br/org/ruds/repositorys/CompanyRepository.java
@@ -1,26 +1,10 @@
 package br.org.ruds.repositorys;
 
-import java.util.Optional;
-
 import br.org.ruds.entitys.Company;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class CompanyRepository implements PanacheRepository<Company> {
     
-    @Inject
-    private CompanyRepository repository;
-
-    public Optional<Company> saveCompany(Company new_) {   
-        try {
-            repository.persist(new_);
-            int listSize = repository.listAll().size();
-            Company persisted = repository.listAll().get(listSize-1);
-            return Optional.of(persisted);
-        } catch (Exception e) {
-            return Optional.empty();
-        }
-    }
 }
--- src/main/java/br/org/ruds/services/CompanyService.java
+++ src/main/java/br/org/ruds/services/CompanyService.java
@@ -1,8 +1,26 @@
 package br.org.ruds.services;
 
+import java.util.Optional;
+
+import br.org.ruds.entitys.Company;
+import br.org.ruds.repositorys.CompanyRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 
 @ApplicationScoped
 public class CompanyService {
     
+    @Inject
+    private CompanyRepository repository;
+
+    public Optional<Company> save(Company new_) {   
+        try {
+            repository.persist(new_);
+            int listSize = repository.listAll().size();
+            Company persisted = repository.listAll().get(listSize-1);
+            return Optional.of(persisted);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
 }
