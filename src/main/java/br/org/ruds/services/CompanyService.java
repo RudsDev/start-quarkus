93e8ec6d5a5029d0136023a3be3b6a62e09b7c2c
Rafael Ulisses <ruds.devel@gmail.com>
2023-04-29 19:08:03 -0300

    Implemented CompanyService.getAll

--- src/main/java/br/org/ruds/services/CompanyService.java
+++ src/main/java/br/org/ruds/services/CompanyService.java
@@ -1,5 +1,6 @@
 package br.org.ruds.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import br.org.ruds.entitys.Company;
@@ -19,6 +20,15 @@ public class CompanyService {
             int listSize = repository.listAll().size();
             Company persisted = repository.listAll().get(listSize-1);
             return Optional.of(persisted);
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<List<Company>> getAll() {   
+        try {
+            List<Company> list = repository.listAll();
+            return Optional.of(list);
         } catch (Exception e) {
             return Optional.empty();
         }
