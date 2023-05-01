62ecf4157eaa61be2e0fa3106b3ade66a258844e
Rafael Ulisses <ruds.devel@gmail.com>
2023-05-01 08:45:16 -0300

    Tests created into CompanyControllerTest

--- src/test/java/br/org/ruds/controllers/CompanyControllerTest.java
+++ src/test/java/br/org/ruds/controllers/CompanyControllerTest.java
@@ -1,5 +1,54 @@
 package br.org.ruds.controllers;
 
+import org.junit.jupiter.api.Test;
+
+import br.org.ruds.entitys.Company;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.ws.rs.core.Response.Status;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+@QuarkusTest
 public class CompanyControllerTest {
+    final private String PREFIX = "/company";
     
+    @Test
+    public void testSaveCompanyEndpoint() throws Exception {
+        Company company = createCompany();
+
+        given()
+            .contentType("application/json")
+            .body(company)
+            .when()
+            .post(PREFIX)
+                .then()
+                .statusCode(Status.CREATED.getStatusCode())
+                .body("name", equalTo("Test Company"))
+                .body("registry", equalTo("0000000"))
+                .body("address", equalTo("Street Test, 000"))
+                .body("phone", equalTo("+00 00 00000000"));
+
+    }
+
+    @Test
+    public void testFindAllEndpoint() throws Exception {
+
+        when()
+            .get(PREFIX)
+                .then()
+                .statusCode(Status.OK.getStatusCode())
+                .body("size()", greaterThanOrEqualTo(0));
+    }
+
+    private Company createCompany() {
+        Company company = new Company();
+        company.setName("Test Company");
+        company.setRegistry("0000000");
+        company.setAddress("Street Test, 000");
+        company.setPhone("+00 00 00000000");
+        return company;
+    }
 }
